71fe31de2d56982562808a6930c172be50a6fd15
NamTaeHo <han127kr@gmail.com>
2024-05-10 20:03:35 +0900

    JSX UI 표현

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import "./Main.css";
+
 //JSX 주의사항
 // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다.
 // 2. 숫자, 문자열, 배열 값만 렌더링된다.
@@ -5,16 +7,26 @@
 // 4. 최상위 태그는 반드시 하나여야만 한다.
 
 const Main = () => {
-  const number = 16;
-  const obj = { a: 1 };
+  const user = {
+    name: "이정환",
+    isLogin: true,
+  };
 
-  return (
-    <main>
-      <h1>main</h1>
-      <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-      {obj.a}
-    </main>
-  );
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+
+  // return (
+  //   <>
+  //     {user.isLogin ? (
+  //       <div className="logout">로그아웃</div>
+  //     ) : (
+  //       <div>로그인</div>
+  //     )}
+  //   </>
+  // );
 };
 
 export default Main;
